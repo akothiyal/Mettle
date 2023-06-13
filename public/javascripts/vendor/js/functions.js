99a23fb5220f39e58ac6b386797e0c6810d50619
chinmayshewale <shewalechinmay54@gmail.com>
2023-06-14 04:40:11 +0530

    Fresh start

--- public/javascripts/vendor/js/functions.js
+++
@@ -1,3 +0,0 @@
-function setCurrentMenuItem(MenuItemID){
-	document.getElementById(MenuItemID).style.background-color='red';
-}
--- public/javascripts/vendor/js/menu-bar-hover.js
+++
@@ -1,5 +0,0 @@
-$('ul.nav li.dropdown').hover(function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(0).fadeIn();
-}, function() {
-  $(this).find('.dropdown-menu').stop(true, true).delay(0).fadeOut();
-});
